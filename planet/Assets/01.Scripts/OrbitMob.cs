3cdab041f2b88e0a4710c37637ffda97d0c9b957
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2023-05-09 22:30:32 +0900

    05 09

--- planet/Assets/01.Scripts/OrbitMob.cs
+++ planet/Assets/01.Scripts/OrbitMob.cs
@@ -23,12 +23,12 @@ public class OrbitMob : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.gameObject.name == "Square") {
         Debug.Log("Colliderd " + collision.name);
         enemyController.GetComponent<EnemyController>().AttackEnemy(Damage);
-        // 뭐에 충돌했는지를 인지를 못함 if 문 쓰면 될듯
         enemyController.GetComponent<EnemyController>().KillEnemy(Gold);
 
         Destroy(this.gameObject);
-
+        }
     }
 }
